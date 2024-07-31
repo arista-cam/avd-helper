41c070c2c7ce80e11c6e6b2e2700899d5b3b2425
CameronPrior <cameron.prior85@gmail.com>
2024-07-31 22:50:02 +0000

    Updates to install.sh script

--- install.sh
+++ install.sh
@@ -9,68 +9,38 @@ fi
 
 install_docker() {
     echo "Installing Docker..."
-    # Update the package index
     sudo apt-get update
-
-    # Install required packages
-    sudo apt-get install -y \
-        apt-transport-https \
-        ca-certificates \
-        curl \
-        gnupg \
-        lsb-release
-
-    # Add Docker’s official GPG key
+    sudo apt-get install -y apt-transport-https ca-certificates curl gnupg lsb-release
     curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-
-    # Check if OS is Debian or Ubuntu and add the appropriate repository
-    . /etc/os-release
     if [ "$ID" = "debian" ]; then
-        echo \
-        "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian \
-        $VERSION_CODENAME stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+        echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian $VERSION_CODENAME stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
     elif [ "$ID" = "ubuntu" ]; then
-        echo \
-        "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
-        $VERSION_CODENAME stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+        echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $VERSION_CODENAME stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
     elif [ "$ID" = "linuxmint" ]; then
-        echo \
-        "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
-        $UBUNTU_CODENAME stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null    
+        echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $UBUNTU_CODENAME stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null    
     else
         echo "Unsupported OS: $ID"
         exit 1
     fi
-
-    # Update the package index again
     sudo apt-get update
-
-    # Install Docker Engine
     sudo apt-get install -y docker-ce docker-ce-cli containerd.io
-
-    # Verify the Docker installation
     sudo docker --version
 }
 
 install_containerlab() {
     echo "Installing Containerlab..."
-    # Install Containerlab
     sudo bash -c "$(curl -sL https://get.containerlab.dev)"
-    
-    # Verify the Containerlab installation
     containerlab version
 }
 
 install_python() {
     echo "Installing Python..."
-    # Install Python
     sudo apt-get update
     sudo apt-get install -y python3
 }
 
 install_pip() {
     echo "Installing pip..."
-    # Install pip using get-pip.py with --break-system-packages switch
     curl -sS https://bootstrap.pypa.io/get-pip.py -o get-pip.py
     sudo python3 get-pip.py --break-system-packages
     rm get-pip.py
@@ -78,13 +48,11 @@ install_pip() {
 
 install_ansible() {
     echo "Installing Ansible..."
-    # Install Ansible using pip with --break-system-packages switch
     sudo python3 -m pip install ansible-core --break-system-packages
 }
 
 install_avd_collection() {
     echo "Installing Arista AVD Collection..."
-    # Install Arista AVD Collection
     sudo ansible-galaxy collection install arista.avd
 }
 
@@ -96,8 +64,6 @@ install_pyavd() {
 install_other_dependencies() {
     echo "Installing other dependencies..."
     sudo apt install -y python3-netaddr
-
-    # Install dependencies from requirements.txt
     if [ -f requirements.txt ]; then
         sudo pip3 install -r requirements.txt --break-system-packages
     else
@@ -105,42 +71,19 @@ install_other_dependencies() {
     fi
 }
 
-if [ "$DOCKER_REQUIRED" == "true" ]; then
-    install_docker
-fi
-
-if [ "$CONTAINERLAB_REQUIRED" == "true" ]; then
-    install_containerlab
-fi
-
-if [ "$PYTHON_REQUIRED" == "true" ]; then
-    install_python
-fi
-
-if [ "$PIP_REQUIRED" == "true" ]; then
-    install_pip
-fi
-
-if [ "$ANSIBLE_REQUIRED" == "true" ]; then
-    install_ansible
-fi
-
-if [ "$AVD_COLLECTION_REQUIRED" == "true" ]; then
-    install_avd_collection
-fi
-
-if [ "$PYAVD_REQUIRED" == "true" ]; then
-    install_pyavd
-fi
-
-if [ "$CVPRAC_REQUIRED" == "true" ] || [ "$REQUESTS_REQUIRED" == "true" ] || [ "$DOCKER_PY_REQUIRED" == "true" ] || [ "$PARAMIKO_REQUIRED" == "true" ]; then
-    install_other_dependencies
-fi
+[ "$DOCKER_REQUIRED" == "true" ] && install_docker
+[ "$CONTAINERLAB_REQUIRED" == "true" ] && install_containerlab
+[ "$PYTHON_REQUIRED" == "true" ] && install_python
+[ "$PIP_REQUIRED" == "true" ] && install_pip
+[ "$ANSIBLE_REQUIRED" == "true" ] && install_ansible
+[ "$AVD_COLLECTION_REQUIRED" == "true" ] && install_avd_collection
+[ "$PYAVD_REQUIRED" == "true" ] && install_pyavd
+([ "$CVPRAC_REQUIRED" == "true" ] || [ "$REQUESTS_REQUIRED" == "true" ] || [ "$DOCKER_PY_REQUIRED" == "true" ] || [ "$PARAMIKO_REQUIRED" == "true" ]) && install_other_dependencies
 
 if [ "$RESTART_SCRIPT" == "true" ]; then
     export INSTALL_SCRIPT_RUN=1
-    # Restart the Python script
-    exec sudo python3 avd_helper.py
+    echo "Restarting the Python script..."
+    exec python3 avd_helper.py
 else
     echo "Installation script completed."
     echo "You can now run the avd_helper.py script"
